093b3431711754973a6581f8f8a06ac25f896e9d
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-10-31 23:44:49 -0700

    updates

--- clicks-per-rust/src/main.rs
+++ clicks-per-rust/src/main.rs
@@ -1,6 +1,7 @@
 use dioxus::prelude::*;
 //use dioxus_logger::tracing::Level; ???
 //use nfd::{open_file_dialog, Response}; // imports file dialog windows for rust cross platform
+use std::time::{SystemTime, UNIX_EPOCH}; // for system time
 
 // declaration of a prop
 #[derive(Props, Clone, PartialEq)] // marcos
@@ -22,10 +23,16 @@ fn Notes() -> Element {
 fn test(event: Event<MouseData>) {
     //println!("{event:?}"); // printing despite type mismatch
     log::info!("Event thing: {event:?}"); // yes you need a crate called log (A rust universal)
+    start_timer();
 }
 
 fn start_timer() {
-
+    let system_time = SystemTime::now();
+    // Check if time is greater than UNIX_EPOCH to avoid errors
+    match system_time.duration_since(UNIX_EPOCH) {
+        Ok(duration) => log::info!("System time in seconds since UNIX_EPOCH: {}", duration.as_secs()),
+        Err(e) => log::info!("Error getting system time: {:?}", e),
+    }
 }
 
 // props with componet
@@ -39,8 +46,6 @@ fn Notes1(props: CustomProps) -> Element {
 fn app() -> Element {
     log::info!("startup log");
     let mut count = use_signal(||0); // creates new var init with 0 ( HOOK )
-
-    let mut email = use_signal(|| String::from("")); // init with empty sting
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
         p {class: "white", "Testing 1 2 3" } 
