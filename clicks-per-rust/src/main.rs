ee5f9fd4e86e24c6adf4de1f31c80f3edd2e8592
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-01 21:31:49 -0700

    Update main.rs

--- clicks-per-rust/src/main.rs
+++ clicks-per-rust/src/main.rs
@@ -5,20 +5,13 @@ use dioxus::prelude::*;
 use chrono::Timelike;
 //use chrono::{DateTime, Local};
 
-// declaration of a prop
-#[derive(Props, Clone, PartialEq)] // marcos
-struct CustomProps {
-    text: String,
-    #[props(optional)] // makes props under optional 
-    size: i32
-}
-
-fn time_handler(intial_time: chrono::DateTime<chrono::Local>) -> bool {
+//fn time_handler(intial_time: chrono::DateTime<chrono::Local>) -> bool {
+fn time_handler(intial_time: u32) -> bool {
     //let current_string = chrono::Local::now().to_string();
     //let mut seconds = chrono::TimeDelta::seconds(10);
-    let parsed_intial:i32 = intial_time.to_string().parse().unwrap();
+    let parsed_intial:u32 = intial_time.to_string().parse().unwrap();
     let current_time = chrono::Local::now().second();
-    let mut parsed_current:i32 = current_time.to_string().parse().unwrap();
+    let mut parsed_current:u32 = current_time.to_string().parse().unwrap();
     let end_time = parsed_intial + 5; // Todo, don't forget about 60 seconds in a min, reseting intial
     while parsed_current < end_time {
         parsed_current = chrono::Local::now().second().to_string().parse().unwrap();
@@ -26,33 +19,25 @@ fn time_handler(intial_time: chrono::DateTime<chrono::Local>) -> bool {
     return true;
 }
 
-// props with componet
-#[component]
-fn Notes1(props: CustomProps) -> Element {
-    //let current_time = chrono::Local::now();
-    rsx! {
-        p { "{props.text}" }
-        //p { "{current_time.second()}" }
-    }
-}
-
 fn app() -> Element {
     log::info!("startup log");
+    let mut started = use_signal(||false);
     let timer_fin = use_signal(||false);
     let mut count = use_signal(||0); // creates new var init with 0 ( HOOK )
     let mut intial_time = use_signal(||chrono::Local::now().second());
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
-        p {class: "white", "Testing 1 2 3" } 
-        Notes1 {text: "test from  struct"}
 
         p { "current seconds: {intial_time} "}
 
         button {
             onclick: move |_event | {
+                if started == use_signal(||false) {
+                    started.set(true);
+                }
                 //intial_time = use_signal(||chrono::Local::now().second()); you can not use use_signal, use set
-                //time_handler(chrono::Local::now());
                 intial_time.set(chrono::Local::now().second());
+                time_handler(chrono::Local::now().second());
             },
             "start timer"
         }
