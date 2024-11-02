88dde5ca72934449e94c52fd120bf03f0cf8146c
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-01 19:55:26 -0700

    Update main.rs

--- clicks-per-rust/src/main.rs
+++ clicks-per-rust/src/main.rs
@@ -1,7 +1,9 @@
 use dioxus::prelude::*;
 //use dioxus_logger::tracing::Level; ???
 //use nfd::{open_file_dialog, Response}; // imports file dialog windows for rust cross platform
+//use chrono::*;
 use chrono::Timelike;
+//use chrono::{DateTime, Local};
 
 // declaration of a prop
 #[derive(Props, Clone, PartialEq)] // marcos
@@ -11,28 +13,17 @@ struct CustomProps {
     size: i32
 }
 
-// basic componet
-#[component]
-fn Notes() -> Element {
-    rsx! {
-        "Notes component"
+fn time_handler(intial_time: chrono::DateTime<chrono::Local>) -> bool {
+    //let current_string = chrono::Local::now().to_string();
+    //let mut seconds = chrono::TimeDelta::seconds(10);
+    let parsed_intial:i32 = intial_time.to_string().parse().unwrap();
+    let current_time = chrono::Local::now();
+    let mut parsed_current:i32 = current_time.to_string().parse().unwrap();
+    let end_time = parsed_intial + 5;
+    while parsed_current > end_time {
+        parsed_current = chrono::Local::now().to_string().parse().unwrap();
     }
-}
-
-// testing fuction
-fn test(event: Event<MouseData>) {
-    //println!("{event:?}"); // printing despite type mismatch
-    log::info!("Event thing: {event:?}"); // yes you need a crate called log (A rust universal)
-}
-
-fn time_handler(intial_time) {
-    let end_time = intial_time + 5 ;
-    let mut current_time = chrono::Local::now();
-    while (current_time < end_time) {
-        current_time = chrono::Local::now();
-    }
-    return;
-
+    return true;
 }
 
 // props with componet
@@ -47,25 +38,26 @@ fn Notes1(props: CustomProps) -> Element {
 
 fn app() -> Element {
     log::info!("startup log");
+    let mut timer_fin = use_signal(||false);
     let mut count = use_signal(||0); // creates new var init with 0 ( HOOK )
-    let mut intial_time = chrono::Local::now(); 
+    let mut intial_time = use_signal(||chrono::Local::now());
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
         p {class: "white", "Testing 1 2 3" } 
-        Notes {}
         Notes1 {text: "test from  struct"}
 
         button {
-            onclick: move |event | {
-                intial_time = chrono::Local::now();
-                time_handler(intial_time);
-            }
+            onclick: move |_event | {
+                intial_time = use_signal(||chrono::Local::now());
+                time_handler(chrono::Local::now());
+            },
+            "start timer"
         }
+        p { " timer_fin: {timer_fin}" }
 
         // button tied to event handeler
         button {
-            onclick: move |event | { 
-                test(event);
+            onclick: move |_event | { 
                 count+=1;
             },
             "Click me!"
