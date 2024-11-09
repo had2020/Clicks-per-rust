e786e37783ab43903d225798ed1426862dadf14a
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-08 23:03:35 -0800

    Update main.rs

--- clicks-per-rust/src/main.rs
+++ clicks-per-rust/src/main.rs
@@ -6,48 +6,68 @@ use chrono::Timelike;
 use tokio;
 //use chrono::{DateTime, Local};
 
+static mut TIME_END: bool = false;
+
 fn app() -> Element {
     log::info!("startup log");
 
-    let mut count = use_signal(||0.0); // creates new var init with 0 ( HOOK )
-    let mut intial_time = use_signal(||chrono::Local::now().second());
-    let mut nocounting = use_signal(||true);
-    let mut not_ended = use_signal(||true);
+    let mut count = use_signal(|| 0.0); // creates new var init with 0 ( HOOK )
+    //let mut intial_time = use_signal(|| chrono::Local::now().second());
+    let mut nocounting = use_signal(|| true);
+    let mut not_ended = use_signal(|| true);
 
-    let mut last_current_time = use_signal(||chrono::Local::now().second());
-    let mut last_end_time = use_signal(||chrono::Local::now().second());
+    let mut last_current_time = use_signal(|| chrono::Local::now().second());
+    let mut last_end_time = use_signal(|| chrono::Local::now().second());
 
-
-    let mut cps = use_signal(||0.0);
+    let mut cps = use_signal(|| 0.0);
     let mut cps_float = 0.0_f32;
 
-    let test_future = use_future(///     use_future(move || async move {
-        ///         loop {
-        ///            if running() {
-        ///                count += 1;
-        ///            }
-        ///            tokio::time::sleep(Duration::from_millis(400)).await;
-        ///        })
+    let mut timer_ended = use_signal(|| false);
 
+    let exp = move || {
+        spawn(async move {
+            unsafe {TIME_END = true}
+        });
+    };
+    
+    /*
+    let mut future1 = use_resource(|| async move {
+        let mut intial_time = use_signal(|| chrono::Local::now().second());
+        let parsed_initial: u32 = (intial_time()).to_string().parse().unwrap();
+        let current_time = chrono::Local::now().second();
+        let parsed_current: u32 = current_time.to_string().parse().unwrap();
+        let mut end_time = parsed_initial + 5;
+        if end_time > 54 {
+            end_time = 0;
+        }
+        while current_time != end_time {
+            current_time = chrono::Local::now().second();
+        }
+        unsafe { TIME_END = true }
+    });
+    */
+    /*
     spawn(async {
         let _ = tokio::spawn(async {}).await;
-    
+
         let _ = tokio::task::spawn_local(async {
             // some !Send work
         })
         .await;
     });
+    */
 
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
-        
-        //p {"time started: {intial_time}"} // debug 
+
+        //p {"time started: {intial_time}"} // debug
 
         if (*not_ended)() {
             button {
                 onclick: move |_event | {
-                    if nocounting() { 
-    
+                    /* 
+                    if nocounting() {
+
                         intial_time.set(chrono::Local::now().second());
                         nocounting.set(false);
                     } else {
@@ -55,12 +75,12 @@ fn app() -> Element {
                         let parsed_initial:u32 = (intial_time()).to_string().parse().unwrap(); //error somewhere here!
                         let current_time = chrono::Local::now().second(); // TODO forget errors make a total counted and count the change in seconds place, for timer
                         let parsed_current:u32 = current_time.to_string().parse().unwrap();
-                        let mut end_time = parsed_initial + 5; 
+                        let mut end_time = parsed_initial + 5;
                         if end_time > 54 { // TODO test and fix
                             end_time = 59;
                         }
                         last_end_time.set(end_time);
-                        
+
                         if parsed_current < end_time {
                             count+=1.0;
                             last_current_time.set(parsed_current);
@@ -69,6 +89,9 @@ fn app() -> Element {
                             not_ended.set(false);
                         }
                     }
+                    */
+                    //future1
+                    exp;
                 },
                 " Click Me To Count! "
             }
@@ -110,4 +133,3 @@ fn app() -> Element {
 fn main() {
     launch(app);
 }
-
