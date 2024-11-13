7a56f242414508404ab8bed5a9ae17615b4d92c8
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-13 01:36:49 -0800

    more failure
    It is impossible in rust dixous to have more than two things running at the same time.

--- clicks-per-rust/src/main.rs
+++ clicks-per-rust/src/main.rs
@@ -1,21 +1,47 @@
 use dioxus::prelude::*;
-use chrono::Timelike;
+use chrono::{TimeDelta, Timelike};
 //use tokio::time::{self, Duration};
-use tokio::time::{sleep, Duration};
+//use tokio::time::{sleep, Duration};
+//use tokio::spawn;
+//use async_std::task;
+use async_std::task::spawn;
 
-//static mut TIME_END: bool = false;
+
+static mut TIME_END: bool = false;
+
+async fn timer() -> f32 {
+    //unsafe { TIME_END = true } // DEBUG ONLY
+    let mut current_time = chrono::Local::now().second();
+    let mut end_time = current_time + 5;
+    if end_time > 58 {
+        end_time = 5;
+    }
+    
+    loop {
+        current_time = chrono::Local::now().second();
+        if current_time == end_time {
+            break;
+        }
+        //sleep(Duration::from_millis(100)).await; // short sleep to yield control, otherwise halt 
+    }
+    
+    unsafe { TIME_END = true };
+    0.1 
+}
 
 fn app() -> Element {
     log::info!("startup log");
 
+    let mut signal = use_signal_sync(|| 0.0_f32);
     let mut count = use_signal(|| 0.0); // creates new var init with 0 ( HOOK )
     let mut nocounting = use_signal(|| true);
     let mut not_ended = use_signal(|| true);
 
-    let last_current_time = use_signal(|| chrono::Local::now().second());
-    let last_end_time = use_signal(|| chrono::Local::now().second());
+    let mut result = use_signal(||0.0_f32);
+    //let last_current_time = use_signal(|| chrono::Local::now().second());
+    //let last_end_time = use_signal(|| chrono::Local::now().second());
 
-    let mut cps = use_signal(|| 0.0);
+    //let mut cps = use_signal(|| 0.0);
     let mut cps_float = 0.0_f32;
 
     rsx! {
@@ -26,38 +52,20 @@ fn app() -> Element {
                 onclick: move | _event | {
                     if (nocounting)() == true {
                         nocounting.set(false);
-                        // running parrarell 
+                        //spawn(timer()); not returning only running
                         spawn(async move {
-
-                            let mut current_time = chrono::Local::now().second();
-                            let mut end_time = current_time + 5;
-                            if end_time > 58 {
-                                end_time = 5;
-                            }
-
                             /* 
-                            while current_time != end_time {
-                                current_time = chrono::Local::now().second();
-                            }
-                            */
+                            let _future_cps = timer();
+                            //signal.set(_future_cps.clone()); nope of course not
+                            // Testing and debuging here
+                            let result = _future_cps.await; //works but halts 
+                            //result.set(future_cps.await);
+                            //count+= 999.9;
+                            count.set(result);
+                            //not_ended.set(true);
+                            */ //only without use async_std::task::spawn;
 
-                            while chrono::Local::now().second() != end_time {
-                                
-                            }
-                            
-                            //sleep(Duration::from_secs(5)).await;
-
-                            count+= 999.9;
-                            not_ended.set(false);
-
-                            // debug test, if parrel runing
-                            /* 
-                            count+= 999.9;
-                            for i in 0..=100000{
-                                count+= 10.0;
-                            }
-                            println!("Clicked!");
-                            */
+                            unsafe { TIME_END = true };
                         });
                     }
                 },
@@ -67,6 +75,9 @@ fn app() -> Element {
 
         button {
             onclick: move | _event | {
+                unsafe { if TIME_END == true {
+                    count+=888.8;
+                } }
                 count+= 1.0;
             },
             " Click Me To Count! "
@@ -83,6 +94,7 @@ fn app() -> Element {
                     count.set(0.0);
                     nocounting.set(true);
                     not_ended.set(true);
+                    unsafe { TIME_END = false }
                 },
                 "Test Again"
             }
