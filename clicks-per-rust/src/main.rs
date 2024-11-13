68cf0939b9e156f84bf98a9cfef50e8423b48361
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-13 02:13:59 -0800

    Update main.rs

--- clicks-per-rust/src/main.rs
+++ clicks-per-rust/src/main.rs
@@ -1,48 +1,15 @@
 use dioxus::prelude::*;
 use chrono::{TimeDelta, Timelike};
-//use tokio::time::{self, Duration};
-//use tokio::time::{sleep, Duration};
-//use tokio::spawn;
-//use async_std::task;
-use async_std::task::spawn;
-
-
-static mut TIME_END: bool = false;
-
-async fn timer() -> f32 {
-    //unsafe { TIME_END = true } // DEBUG ONLY
-    let mut current_time = chrono::Local::now().second();
-    let mut end_time = current_time + 5;
-    if end_time > 58 {
-        end_time = 5;
-    }
-    
-    loop {
-        current_time = chrono::Local::now().second();
-        if current_time == end_time {
-            break;
-        }
-        //sleep(Duration::from_millis(100)).await; // short sleep to yield control, otherwise halt 
-    }
-    
-    unsafe { TIME_END = true };
-    0.1 
-}
 
 fn app() -> Element {
     log::info!("startup log");
 
-    let mut signal = use_signal_sync(|| 0.0_f32);
     let mut count = use_signal(|| 0.0); // creates new var init with 0 ( HOOK )
-    let mut nocounting = use_signal(|| true);
+    let mut counting = use_signal(|| false); // for using only one variable
     let mut not_ended = use_signal(|| true);
-
-    let mut result = use_signal(||0.0_f32);
-    //let last_current_time = use_signal(|| chrono::Local::now().second());
-    //let last_end_time = use_signal(|| chrono::Local::now().second());
-
-    //let mut cps = use_signal(|| 0.0);
     let mut cps_float = 0.0_f32;
+    let mut end_second = use_signal(|| 0_u32);
+    let mut end_minute: = use_signal(|| 0_u32);
 
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
@@ -50,41 +17,24 @@ fn app() -> Element {
         if (*not_ended)() {
             button {
                 onclick: move | _event | {
-                    if (nocounting)() == true {
-                        nocounting.set(false);
-                        //spawn(timer()); not returning only running
-                        spawn(async move {
-                            /* 
-                            let _future_cps = timer();
-                            //signal.set(_future_cps.clone()); nope of course not
-                            // Testing and debuging here
-                            let result = _future_cps.await; //works but halts 
-                            //result.set(future_cps.await);
-                            //count+= 999.9;
-                            count.set(result);
-                            //not_ended.set(true);
-                            */ //only without use async_std::task::spawn;
-
-                            unsafe { TIME_END = true };
-                        });
+                    if (counting)() == true {
+                        count+=1.0;
+                    } else {
+                        let current_second = chrono::Local::now().second();
+                        let current_minute = chrono::Local::now().minute();
+                        end_second = current_second + 5;
+                        if current_second > 54 {
+                            end_minute = current_minute + 1;
+                            current_second
+                        } else {
+                            0
+                        };
+                        
                     }
                 },
-                " Click Me To Count! OLD"
+                " Click Me To Count! "
             }
         }
-
-        button {
-            onclick: move | _event | {
-                unsafe { if TIME_END == true {
-                    count+=888.8;
-                } }
-                count+= 1.0;
-            },
-            " Click Me To Count! "
-        }
-
-        //p {"end time: {last_end_time}"} // debug
-       //p {"current time: {last_current_time}"} // debug
 
         if (*not_ended)() {
             p {class: "white", "Clicked : {count}"}
@@ -92,9 +42,8 @@ fn app() -> Element {
             button {
                 onclick: move |_event | {
                     count.set(0.0);
-                    nocounting.set(true);
+                    counting.set(true);
                     not_ended.set(true);
-                    unsafe { TIME_END = false }
                 },
                 "Test Again"
             }
